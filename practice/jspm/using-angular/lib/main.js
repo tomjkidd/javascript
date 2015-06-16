b5edfbcf0176d0c7713883980f29abf0ea5eeb9f
unknown <tomjkidd@gmail.com>
2015-06-15 20:50:04 -0400

    Using the jspm css and json plugins

--- practice/jspm/using-angular/lib/main.js
+++ practice/jspm/using-angular/lib/main.js
@@ -2,6 +2,11 @@ import angular from 'angular'
 import { TestService } from 'lib/TestService'
 import { TestCtrl } from 'lib/TestCtrl'
 
+import 'styles/style.css!'
+import colors from 'api/color-data.json!json'
+
+console.log(colors);
+
 angular
     .module('app', [])
     .factory('TestService', TestService)
