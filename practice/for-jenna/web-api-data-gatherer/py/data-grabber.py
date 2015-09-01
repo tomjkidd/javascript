e0d018520ea0094bf29bea533112e1280878b518
Tom Kidd <tomjkidd@gmail.com>
2015-08-31 22:06:48 -0400

    Adding handling for multiple monitorIds and saving to file

--- practice/for-jenna/web-api-data-gatherer/py/data-grabber.py
+++ practice/for-jenna/web-api-data-gatherer/py/data-grabber.py
@@ -4,12 +4,16 @@ import json
 baseUrl = "https://forsight.crimsonhexagon.com/api/"
 
 def get(url):
-    return urllib2.urlopen(url).read();
+    return urllib2.urlopen(url).read()
 
 def getMonitorDetail(auth, monitorId):
     monitorDetailBaseUrl = baseUrl + "monitor/detail"
     monitorDetailUrl = "{0}?auth={1}&id={2}".format(monitorDetailBaseUrl, auth, monitorId)
     return json.loads(get(monitorDetailUrl))
+
+def saveJsonDataToFile(filename, jsonData):
+    with open(filename, 'w') as f:
+        json.dump(jsonData, f, indent = 4);
 
 def loadSettings():
     with open('settings.tmp') as f:
@@ -19,6 +23,15 @@ def loadSettings():
 if __name__ == '__main__':
     settings = loadSettings()
 
-    monitorDetail = getMonitorDetail(settings["auth"], settings["monitorId"])
+    monitorIds = settings["monitorIds"]
 
-    print monitorDetail["keywords"]
+    jsonData = []
+
+    for monitorId in monitorIds:
+        monitorDetail = getMonitorDetail(settings["auth"], monitorId)
+        print "Adding '{0}' to the list".format(monitorDetail["name"])
+        jsonData.append(monitorDetail)
+
+    print "{0} items loaded from api".format(len(jsonData))
+
+    saveJsonDataToFile(settings["jsonDataFilename"], jsonData)
