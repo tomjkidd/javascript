799a697337a65217e073c20caf9c25402e83e85a
Tom Kidd <tomjkidd@gmail.com>
2015-09-29 09:05:07 -0400

    Adding some more complexity to the ui-router example

--- practice/ui-router-learning/app/app.js
+++ practice/ui-router-learning/app/app.js
@@ -15,15 +15,44 @@
                     templateUrl: 'templates/reporting.details.html'
                 })
                 .state('configuration', {
-                    url: '/configuration',
+                    /*abstract: true,*/
+                    url: '/configuration/{type:int}/{subtype:int}',
+                    /*url: '/configuration',*/
                     templateUrl: 'templates/configuration.html',
-                    controller: function ($scope) {
+                    controller: function ($scope, $stateParams) {
                         $scope.items = ['A', 'List', 'Of', 'Items'];
+                        $scope.type = $stateParams.type;
+                        $scope.subtype = $stateParams.subtype;
+
+                        /*$scope.state = {
+                            type: $stateParams.type,
+                            subtype: $stateParams.subtype
+                        }*/
+
+                        $scope.add1Type = function(){
+                            $scope.type = $scope.type + 1;
+                        }
+                    },
+                    resolve: {
+                        type: ['$stateParams', function($stateParams) {
+                            return $stateParams.type;
+                        }],
+                        subtype: ['$stateParams', function($stateParams) {
+                            return $stateParams.subtype;
+                        }],
                     }
                 })
                 .state('configuration.details', {
                     url: '/details',
-                    templateUrl: 'templates/configuration.details.html'
+                    templateUrl: 'templates/configuration.details.html',
+                    controller: function($scope, $stateParams, type, subtype) {
+                        /*$scope.type = type || 0;
+                        $scope.subtype = subtype || 1;*/
+                    }
+                })
+                .state('configuration.summary', {
+                    url: '/design',
+                    templateUrl: 'templates/configuration.design.html'
                 })
                 .state('administration', {
                     url: '/administration',
